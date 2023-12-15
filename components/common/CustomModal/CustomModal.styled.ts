930d16c80b6fcc81cec27047d9a70eb315914158
Lisnyi <russelhobbbs@gmail.com>
2023-12-15 17:36:26 +0200

    modal update

--- components/common/CustomModal/CustomModal.styled.ts
+++ components/common/CustomModal/CustomModal.styled.ts
@@ -9,6 +9,8 @@ export const styles = StyleSheet.create({
     modal: {
         flex: 1,
         alignItems: 'center',
+        justifyContent: 'center',
+        minWidth: 100,
         maxHeight: 180,
         borderRadius: 20,
         borderWidth: 1,
